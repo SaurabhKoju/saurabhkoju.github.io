be5a4439d1dfcf01e941e6f2407bc138a9f04539
SaurabhKoju <59972140+SaurabhKoju@users.noreply.github.com>
2025-12-15 12:20:28 +0000

    Deploying to gh-pages from @ SaurabhKoju/saurabhkoju.github.io@0a1600e49f34532acdb2c29641c9f9b6b0d7bde8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,11 +16,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/blog/";
           },
+        },{id: "nav-cv",
+          title: "CV",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/assets/pdf/CV.pdf";
+          },
         },{id: "post-flow-matching",
         
           title: "Flow Matching",
         
-        description: "An introduction to Flow Matching",
+        description: "An Introduction to Flow Matching",
         section: "Posts",
         handler: () => {
           
@@ -35,36 +42,17 @@ ninja.data = [{
           window.open("mailto:%6B%6F%6A%75%73%61%75%72%61%62%68@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/SaurabhKoju", "_blank");
+        },
+      },{
         id: 'social-linkedin',
         title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
           window.open("https://www.linkedin.com/in/saurabh-koju-7a71aa208", "_blank");
         },
-      },{
-      id: 'light-theme',
-      title: 'Change theme to light',
-      description: 'Change the theme of the site to Light',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("light");
-      },
-    },
-    {
-      id: 'dark-theme',
-      title: 'Change theme to dark',
-      description: 'Change the theme of the site to Dark',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("dark");
-      },
-    },
-    {
-      id: 'system-theme',
-      title: 'Use system default theme',
-      description: 'Change the theme of the site to System Default',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("system");
-      },
-    },];
+      },];
